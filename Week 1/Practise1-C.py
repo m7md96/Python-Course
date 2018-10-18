e1cd4deedbeb82046946df717380cf012daae36d
m7md96 <44104079+m7md96@users.noreply.github.com>
2018-10-18 09:35:12 -0700

    Add files via upload

---
+++ Week 1/Practise1-C.py
@@ -0,0 +1,8 @@
+# C° = (°F − °32) ÷ 1.8
+# F = (c * 1.8) + 32 
+
+f = int(input("The temperature of Fahrenheit : "))
+print ((f - 32) / 1.8 )
+
+c = int(input("The temperature of Celsius : "))
+print ((c * 1.8) + 32 )
