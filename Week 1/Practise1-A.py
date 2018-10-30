1ecf94d00a99aed35e1a04611750daca9415a028
m7md96 <44104079+m7md96@users.noreply.github.com>
2018-10-31 01:36:21 +0200

    Add files via upload

---
+++ Week 1/Practise1-A.py
@@ -0,0 +1,17 @@
+A = input ("Enter Your Name ")
+b = input ("Enter the first number ")
+c = input ("Enter the second number ")
+
+print("Hi Mr " + A + ",")
+
+print("The Sum of "+str(b)+" and " + str(c) + (" is: ") + str(int(b) + int(c)))
+
+print("The Sub of "+str(b)+" and " + str(c) + (" is: ") + str(int(b) - int(c)))
+
+print("The Mul of "+str(b)+" and " + str(c) + (" is: ") + str(int(b) * int(c)))
+
+print("The Div of "+str(b)+" over " + str(c) + (" is: ") + str(int(b) / int(c)))
+
+print("The Reminder of "+str(b)+" over " + str(c) + (" is: ") + str(int(b) % int(c)))
+
+print("The Exponent of "+str(b)+" to the power of " + str(c) + (" is: ") + str(int(b) ** int(c)))
